2a6b1d19fabb20729dda48197fb3dfb6b0caf309
gaopeng <592908853@qq.com>
2021-01-28 23:38:27 +0800

    10. applyMiddleware thunk,promise,logger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from './react-redux'
+import { Provider } from './react-redux'
 import Counter1 from './components/Counter1'
 import Counter2 from './components/Counter2'
 import store from './store'
+// console.log('store: ', store);
+
+store.subscribe(() => console.log('store', store.getState()));
+
+// redux-promise
+
+store.dispatch(new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve({
+          type:'ADD1',
+          payload:1
+        })
+    })
+}))
+
+
+// retux-thunx
+// console.log('store',store);
+// store.dispatch(({dispatch}) => {
+//   setTimeout(() => {
+//     dispatch({ type: 'MINUS1', payload: 1 })
+//   })
+// })
+
+
 
 //Provider 的原理就是context
 ReactDOM.render(
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { createStore } from '../redux';
-import  rootReducer from './reducers'
+import rootReducer from './reducers'
 
-function applyMiddleWare(logger){
-    return function (createStore){
-        return function(reducer){
+function applyMiddleWare(middleware) {
+    return function (createStore) {
+        return function (reducer) {
             let store = createStore(reducer)
-            let dispatch = logger(store)(store.dispatch)
+            let dispatch = middleware(store)(store.dispatch)
             return {
                 ...store,
                 dispatch
@@ -17,16 +17,83 @@ function applyMiddleWare(logger){
 
 // 以后在创建仓库的时候 不需要传递默认状态了 ，reducer里面赋值
 // let store = createStore(rootReducer)
-
-function logger({getState}){
-    return function(next){
-       return function(action){
+// next指向的是原始的store.dispatch 只能接收普通的对象那个dispatch
+// store.dispatch 是经过中间件包装处理过的 支持promise function 。。。
+function logger({ getState }) {
+    return function (next) {
+        return function (action) {
             console.log('prev state', getState())
             next(action)
-            console.log('next state',getState())
-       }
+            console.log('next state', getState())
+        }
     }
 }
 
-let store = applyMiddleWare(logger)(createStore)(rootReducer)
+// 只要是中间件 格式是定死的 redux-thunk
+function thunk(middlewareAPI) {
+    return function (next) { // next => store.dispatch
+        return function (action) {
+            if (typeof action === 'function') {
+                return action(middlewareAPI)
+            }
+            // 如果不是函数 则不需要自己处理，直接调下一个dispatch方法就行
+            return next(action)
+        }
+    }
+}
+
+function promise(store) {
+    return function (next) { // next => store.dispatch
+        return function (action) {
+            if (typeof action.then === 'function') {
+                return action.then(newAction => store.dispatch(newAction))
+            }
+            return next(action);
+        }
+    }
+}
+
+function middleware(store) {
+    return function (next) {
+        return function (action) {
+            console.log('prev state', store.getState())
+            if (typeof action === 'function') {
+                action({ dispatch: store.dispatch, getState: store.getState })
+            } else if (typeof action.then === 'function') {
+                action.then(newAction => store.dispatch(newAction))
+            } else {
+                next(action)
+            }
+            console.log('next state', store.getState())
+        }
+    }
+}
+
+function applyMiddleWare1(...middlewares) {
+    return function (createStore) {
+        return function (reducer) {
+            let store = createStore(reducer);
+            let dispatch;
+            let middlewareAPI = {
+                getState: store.getState,
+                dispatch: (action) => dispatch(action)
+            }
+            let chin = middlewares.map(middleware => middleware(middlewareAPI))
+            let [promise, thunk, logger] = chin;
+            dispatch = promise(thunk(logger(store.dispatch)))
+            // dispatch = middleware(middlewareAPI)(store.dispatch)
+            return {
+                ...store,
+                dispatch
+            }
+        }
+    }
+
+}
+
+
+
+// let store = applyMiddleWare(middleware)(createStore)(rootReducer)
+let store = applyMiddleWare1(promise, thunk, logger)(createStore)(rootReducer)
+
 export default store
